4b38a84f3c7a68d68e41f9492598c6e94f31cf5d
ernesdumani <ernesdumani@gmail.com>
2025-03-05 17:38:21 +0100

    ernesi
    added cute basic stuff

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html <?php language_attributions(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ds-theme</title>
+</head>
+<body <?php body_class(); ?>>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, possimus!</p>
+   <div id="page" class="site">
+    <header>
+    <section class="topbar">
+        <div class="logo">
+        logo
+        </div>
+        <div class="search-box">
+            search
+        </div>
+    </section>
+    <section class="menu-area">
+        <nav class="menu-area">
+            Menu
+        </nav>
+    </section>
+</header>
+<!-- header -->
+ <div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <section class="hero">
+
+            hero
+            </section>
+            <section class="services">
+                services
+            </section>
+            <section class="home-blog">
+                Blog
+            </section>
+        </main>
+    </div>
+ </div>
+ <!-- body -->
+  <footer class="site-footer">
+    footer
+  </footer>
+   </div>
+</body>
+</html>
+<!                                                                                                                                                                                  -- footer -->
