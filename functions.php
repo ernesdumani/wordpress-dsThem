54b6e2ec4e5f79417f14ed07db99c4e2279db514
ernesdumani <ernesdumani@gmail.com>
2025-03-10 17:47:12 +0100

    ernes
    saved

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php  
+
+function ds_theme_load_scripts(){
+    wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_style('google_fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet', array(), '1.0', true);
+    wp_enqueue_script('deopdown', get_template_directory_uri().'js/dropdown.js'.arra(),'1.0',true);
+
+}
+add_action('wp-enqueue_scripts','ds_theme_load_scripts');
+?>
